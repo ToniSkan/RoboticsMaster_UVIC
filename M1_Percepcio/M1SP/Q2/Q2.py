80d886e11901829c545f7d24302d5f7610bac6ee
Toni Guasch <antoni.guasch@uvic.cat>
2015-11-02 22:28:23 +0100

    Estructura

---
+++ M1_Percepcio/M1SP/Q2/Q2.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+alldata=np.loadtxt('housing.data')
+y = alldata[:,-1]
+#print 'Data points', len(y)
+print y.T
+
+
+
+#alldata [1,1]
+
+
+
+#todas filas columna 1
+#alldata[:,1]
+
+#define rango
+#alldata[0:2,:]  ##elementos entre o y 2 de todas las columnas
+
+
+#alldata[2:0
+#print alldata
+
+
+
+##Cargamos datos descargados desde itnernet
+
